9ca3620be54887e024dc45ffead308241488fb88
Nithin Kumar <sthestar1515@gmail.com>
2026-09-17 12:04:47 +0530

    Update main.py

--- Flask/main.py
+++ Flask/main.py
@@ -42,10 +42,10 @@ def check_all_positions():
 def getprices(items):
     l = []
     for i in items:
-        print(i)
         response = requests.get(f'https://api.dexscreener.com/tokens/v1/solana/{i["ca"]}',headers={"Accept":"*/*"})
         data = list(response.json())
-        l.append(data[0]["fdv"])
+        print(data)
+        l.append(data[0]["fdv"] or None)
     return l
 
 def getitems():
